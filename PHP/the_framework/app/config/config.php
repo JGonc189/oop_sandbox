fe06950a92742fabc15fda4dc5c7ecdbf40ecb86
Jason Goncalves <jgoncalves@protonmail.com>
2018-10-03 22:19:23 -0400

    added header and footer partials

---
+++ PHP/the_framework/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    // app root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'http://localhost:8888/oop_sandbox/php/the_framework');
+
+    //site name
+    define('SITENAME', 'JasonMVC');
